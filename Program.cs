0540081fd1c0672aceb4d69381da6fd722307e70
epic <alexgauthier2204@gmail.com>
2023-03-14 11:44:11 +0100

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   Carre.csproj
    	new file:   Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Carre
+{
+    class Program
+    {
+	static int CalculeCarre(int val)
+	{
+	    return val * val;
+	}
+
+        static void Main(string[] args)
+        {
+	    val test=4;
+	    Console.WriteLine($"ca marche ou pas {CalculeCarre(test)}");
+        }
+
+    }
+}
